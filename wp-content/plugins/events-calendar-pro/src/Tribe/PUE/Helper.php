0491f796f72e23f331e1161cb695ca30529d1b36
James Davis <james.norman.davis@gmail.com>
2017-09-06 08:56:03 +1000

    Add the-events-calendar plugin

---
+++ wp-content/plugins/events-calendar-pro/src/Tribe/PUE/Helper.php
@@ -0,0 +1,10 @@
+<?php
+
+class Tribe__Events__Pro__PUE__Helper {
+
+	/**
+	 * @var string Helper data
+	 */
+	const DATA = '8d9ed3cfeeed125975a4691cbf7fb8e857b09985';
+
+}
---
+++ wp-content/plugins/the-events-calendar-filterbar/src/Tribe/PUE/Helper.php
@@ -0,0 +1,10 @@
+<?php
+
+class Tribe__Events__Filterbar__PUE__Helper {
+
+	/**
+	 * @var string Helper data
+	 */
+	const DATA = '83f873304ece24c04fa499079d4180aae3e0ea45';
+
+}
